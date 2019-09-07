f63da3f3a3e2302ac608a48f67bea618272cf70d
Daniel Leong <me@dhleong.net>
2019-09-07 10:57:23 -0400

    Fill out some more macro expansion tests

--- src/engine/test/wish_engine/runtime_eval_test.cljs
+++ src/engine/test/wish_engine/runtime_eval_test.cljs
@@ -31,8 +31,24 @@
     )
 
   (testing "as->"
-    ;; TODO
-    ))
+    (is (= 42 (eval-form '(as-> (* 10 2) n
+                            (* 2 n)
+                            (+ 2 n)))))))
+
+(deftest cond-forms-test
+  (testing "cond"
+    (is (= :firefly (eval-form '(cond
+                                  (<= 42 9001) :firefly
+                                  (> v 9001) :serenity
+                                  :else :alliance))))
+    (is (= :serenity (eval-form '(cond
+                                   (>= 42 9001) :firefly
+                                   (< 42 9001) :serenity
+                                   :else :alliance))))
+    (is (= :alliance (eval-form '(cond
+                                   (>= 42 9001) :firefly
+                                   (= 42 9001) :serenity
+                                   :else :alliance))))))
 
 (deftest if-forms-test
   (testing "if-let"
@@ -41,5 +57,10 @@
                             9001))))
     (is (= 9001 (eval-form '(if-let [n false]
                               (* 2 n)
-                              9001))))))
+                              9001)))))
+
+  (testing "if-not"
+    (is (= "true" (eval-form '(if-not true
+                                "false"
+                                "true"))))))
 
