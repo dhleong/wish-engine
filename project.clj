80e43fdca6097fefbf3eab959967a3cad27249e0
Daniel Leong <me@dhleong.net>
2019-09-11 09:54:11 -0400

    Add project.clj for publishing jars

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject wish-engine "0.1.0-SNAPSHOT"
+  :url "https://github.com/dhleong/wish-engine"
+
+  :dependencies
+  ;; always use "provided" for Clojure(Script)
+  [[org.clojure/clojurescript "1.10.520" :scope "provided"]]
+
+  :source-paths
+  ["src/engine/main"])
