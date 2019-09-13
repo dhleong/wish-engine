7ebe815a7399356ecae09e5df4c646e21e3ccf06
Daniel Leong <me@dhleong.net>
2019-09-13 10:06:44 -0400

    Update wish-test-helper to fix advanced tests

--- src/ci/main/wish_test_helper/engine.cljs
+++ src/ci/main/wish_test_helper/engine.cljs
@@ -1,11 +1,13 @@
 (ns wish-test-helper.engine
   "This namespace needs to be distinct so it doesn't get overwritten
    by the wish_engine definition from the karma test file"
-  (:require [wish-engine.core :as core]))
+  (:require [wish-engine.core :as core]
+            [wish-engine.model :as m]))
 
 (defn ^:export eval-string
   ([^String s] (eval-string nil s))
   ([state, ^String s]
    (let [engine (core/create-engine)
-         state (or state (core/create-state engine))]
-     (core/load-source engine state s))))
+         state (or state (core/create-state engine))
+         form (m/parse-string engine (str "(do " s ")"))]
+     (m/eval-source-form engine state form))))
