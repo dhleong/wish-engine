339476cb65afd9d9e8ff9b6ba32e4ceb213c2a3c
Daniel Leong <me@dhleong.net>
2020-04-25 14:16:44 -0400

    Attempt to generate docs for Github Pages

--- project.clj
+++ project.clj
@@ -3,8 +3,12 @@
 
   :dependencies
   ;; always use "provided" for Clojure(Script)
-  [[org.clojure/clojurescript "1.10.520" :scope "provided"]
+  [[org.clojure/clojure "1.10.1" :scope "provided"]
+   [org.clojure/clojurescript "1.10.742" :scope "provided"]
    [com.rpl/specter "1.1.3"]]
+
+  :codox {:language :clojurescript}
+  :plugins [[lein-codox "0.10.7"]]
 
   :source-paths
   ["src/engine/main"])
